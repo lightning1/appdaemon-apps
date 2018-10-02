7577fd4ce065ad70f0e619f475d3e1a5960ec52a
Felix Kluge <f.kluge@agdsn.de>
2018-10-02 11:59:18 +0200

    Fix definition of users in global configuration

--- apps/globals.py
+++ apps/globals.py
@@ -1,9 +1,9 @@
 # Variables that need to be tracked globally
-users = [{
+users = {
         "Max Mustermann": {
             # notification component (has to be telegram)
             "notify": "notify.telegram_max",
             # list of device_tracker entries that belong to the user
             "tracked_devices": ["device_tracker.max_handy"],
         }
-    }]
+    }
