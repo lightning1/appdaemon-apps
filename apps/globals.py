0a71defd93c8ce10fb405915930440fbad07a70f
Felix Kluge <f.kluge@agdsn.de>
2018-10-05 13:41:30 +0200

    Update app example configuration

--- apps/globals.py
+++ apps/globals.py
@@ -5,5 +5,9 @@ users = {
             "notify": "notify.telegram_max",
             # list of device_tracker entries that belong to the user
             "tracked_devices": ["device_tracker.max_handy"],
-        }
+        },
+        "Manuela Musterfrau": {
+            "notify": "notify.telegram_manuela",
+            "tracked_devices": ["device_tracker.manuela_handy", "device_tracker.manuela_tablet"],
+        },
     }
